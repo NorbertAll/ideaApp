8bdf5a79f06fc912e9111c1fc26d87b67d0e6646
NorbertAll <norbert_gutkowski@interia.pl>
2022-08-14 23:37:05 +0200

    add like button on frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,15 +11,22 @@ function Home() {
      axios.get("http://localhost:3001/ideas").then((response)=>{
         setListOfIdeas(response.data)
      }) 
-    }, [])
+    }, []);
+
+  const likeIdea= (ideaId)=> {
+    axios.post("http://localhost:3001/likes", {IdeaId: ideaId}, {headers: {accessToken:localStorage.getItem("accessToken")}}).then((response)=>{
+        alert(response.data)
+     }) 
+  }
+
   return (
     <div>
         <div className="content">
         {listOfIdeas.map((value, key)=>{
-            return <div className="idea" key={value.id} onClick={()=>{navigate(`/idea/${value.id}`)}}>
+            return <div className="idea" key={value.id} >
               <div className='title'>{value.title}</div>
-              <div className='body'>{value.ideaText}</div>
-              <div className='footer'>{value.username}</div>
+              <div className='body'  onClick={()=>{navigate(`/idea/${value.id}`)}}>{value.ideaText}</div>
+              <div className='footer'>{value.username} <button onClick={()=>likeIdea(value.id)}>Like</button></div>
             </div>
           })}
         </div>
